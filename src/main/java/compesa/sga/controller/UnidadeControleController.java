7f2bcafad2fac3645df5294a430704b3a986185f
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-08-21 16:41:26 -0300

    Criacao do endpoint PATCH

--- src/main/java/compesa/sga/controller/UnidadeControleController.java
+++ src/main/java/compesa/sga/controller/UnidadeControleController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,16 +27,16 @@ public class UnidadeControleController {
   @Autowired
   private UnidadeControleService unidadeControleService;
 
-  // 1º EndPoint
+  // 1º ENDPOINT
   @GetMapping
   public ResponseEntity<List<UnidadeControleDto>> GetUnidadeControle() {
-    return ResponseEntity.ok().body(unidadeControleService.GetAllUnidadeControle());
+    return ResponseEntity.ok().body(unidadeControleService.getAllUnidadeControle());
   }
 
-  // 2º EndPoint
+  // 2º ENDPOINT
   @GetMapping("/{id}")
   public ResponseEntity<UnidadeControleDto> GetById(@PathVariable("id") int id) {
-    UnidadeControleDto unidadeControle = unidadeControleService.GetUnidadeControleById(id);
+    UnidadeControleDto unidadeControle = unidadeControleService.getUnidadeControleById(id);
 
     if (Objects.isNull(unidadeControle)) {
       return ResponseEntity.notFound().build();
@@ -43,29 +44,30 @@ public class UnidadeControleController {
     return ResponseEntity.ok().body(unidadeControle);
   }
 
-  // 4º EndPoint
+  // 4º ENDPOINT
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> DeleteUnidadeControle(@PathVariable("id") int id) {
-    UnidadeControleDto unControleDto = unidadeControleService.GetUnidadeControleById(id);
+    UnidadeControleDto unControleDto = unidadeControleService.getUnidadeControleById(id);
 
     if (Objects.isNull(unControleDto)) {
       return ResponseEntity.notFound().build();
     }
 
-    unidadeControleService.DeleteUnidadeControle(id);
+    unidadeControleService.deleteUnidadeControle(id);
 
     return ResponseEntity.noContent().build();
   }
 
-  // 4º EndPoint
+  // 4º ENDPOINT
   @PostMapping
   public ResponseEntity<UnidadeControle> PostUnidadeControle(@RequestBody UnidadeControle unidadeControle) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(unidadeControleService.SaveUnidadeControle(unidadeControle));
+    return ResponseEntity.status(HttpStatus.CREATED).body(unidadeControleService.saveUnidadeControle(unidadeControle));
   }
 
+  // 6º ENDPOINT
   @PutMapping("/{id}")
   public ResponseEntity<UnidadeControle> PutUnidadeControle(@PathVariable("id") int id, @RequestBody UnidadeControle unidadeControle){
-    UnidadeControleDto  uniControle = unidadeControleService.GetUnidadeControleById(id);
+    UnidadeControleDto  uniControle = unidadeControleService.getUnidadeControleById(id);
 
     if (Objects.isNull(uniControle)){
       return ResponseEntity.noContent().build();
@@ -73,8 +75,15 @@ public class UnidadeControleController {
 
     unidadeControle.setId(id);
 
-    return ResponseEntity.ok().body(unidadeControleService.SaveUnidadeControle(unidadeControle));
+    return ResponseEntity.ok().body(unidadeControleService.saveUnidadeControle(unidadeControle));
 
+  }
+
+  // 7º ENDPOINT
+  @PatchMapping("/{id}")
+  public ResponseEntity<UnidadeControle> atualizarTipoControleParcialmente(@PathVariable int id, @RequestBody UnidadeControle atualizacao){
+    UnidadeControle dadosAtualizado = unidadeControleService.uptadeParcial(id, atualizacao);
+    return ResponseEntity.ok(dadosAtualizado);
   }
 
 }
