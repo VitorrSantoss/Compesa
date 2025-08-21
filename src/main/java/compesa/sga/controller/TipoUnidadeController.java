68734a57554f2b63edd8411167a5d2428eaf10d3
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-08-21 15:36:29 -0300

    Criacao do endpoint PATCH

--- src/main/java/compesa/sga/controller/TipoUnidadeController.java
+++ src/main/java/compesa/sga/controller/TipoUnidadeController.java
@@ -8,16 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import compesa.sga.dto.TipoUnidadeDto;
 import compesa.sga.model.TipoUnidade;
 import compesa.sga.services.TipoUnidadeService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/tipoUnidade")
@@ -28,59 +29,63 @@ public class TipoUnidadeController {
 
   // 1º ENDPOINT
   @GetMapping
-  public ResponseEntity<List<TipoUnidadeDto>> GetAllTipoUnidade(){
-    return ResponseEntity.ok().body(tipoUnidadeService.GetAllTipoUnidadeService());
+  public ResponseEntity<List<TipoUnidadeDto>> GetAllTipoUnidade() {
+    return ResponseEntity.ok().body(tipoUnidadeService.getAllTipoUnidadeService());
   }
 
   // 2º ENDPOINT
   @GetMapping("/{id}")
-  public ResponseEntity<TipoUnidadeDto> GetByIdTipoUnidade(@PathVariable("id") int id){
-    TipoUnidadeDto tipoUnidade = tipoUnidadeService.GetTipoUnidadeById(id);
+  public ResponseEntity<TipoUnidadeDto> GetByIdTipoUnidade(@PathVariable("id") int id) {
+    TipoUnidadeDto tipoUnidade = tipoUnidadeService.getTipoUnidadeById(id);
 
-    if (Objects.isNull(tipoUnidade)){
+    if (Objects.isNull(tipoUnidade)) {
       return ResponseEntity.notFound().build();
     }
 
     return ResponseEntity.ok().body(tipoUnidade);
-    
+
   }
 
   // 3º ENDPOINT
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> DeleteTipoUnidade(@PathVariable("id") int id){
-    TipoUnidadeDto tUnidadeDto = tipoUnidadeService.GetTipoUnidadeById(id);
+  public ResponseEntity<Void> DeleteTipoUnidade(@PathVariable("id") int id) {
+    TipoUnidadeDto tUnidadeDto = tipoUnidadeService.getTipoUnidadeById(id);
 
     if (Objects.isNull(tUnidadeDto)) {
       return ResponseEntity.notFound().build();
     }
 
-    tipoUnidadeService.DeleteTipoUnidade(id);
+    tipoUnidadeService.deleteTipoUnidade(id);
 
     return ResponseEntity.noContent().build();
   }
 
-
   // 4º ENDPOINT
   @PostMapping
-  public ResponseEntity<TipoUnidade> PostTipoUnidade(@RequestBody TipoUnidade tipoUnidade){
-    return ResponseEntity.status(HttpStatus.CREATED).body(tipoUnidadeService.SaveTipoUnidade(tipoUnidade));
+  public ResponseEntity<TipoUnidade> PostTipoUnidade(@RequestBody TipoUnidade tipoUnidade) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(tipoUnidadeService.saveTipoUnidade(tipoUnidade));
   }
-
 
   // 5º ENDPOINT
   @PutMapping("/{id}")
-  public ResponseEntity<TipoUnidade> PutTipoUnidade(@PathVariable("id") int id, @RequestBody TipoUnidade tipoUnidade){
-    TipoUnidadeDto tDto = tipoUnidadeService.GetTipoUnidadeById(id);
+  public ResponseEntity<TipoUnidade> PutTipoUnidade(@PathVariable("id") int id, @RequestBody TipoUnidade tipoUnidade) {
+    TipoUnidadeDto tDto = tipoUnidadeService.getTipoUnidadeById(id);
 
-    if(Objects.isNull(tDto)){
+    if (Objects.isNull(tDto)) {
       return ResponseEntity.noContent().build();
     }
 
     tipoUnidade.setId(id);
 
-    return ResponseEntity.ok().body(tipoUnidadeService.SaveTipoUnidade(tipoUnidade));
+    return ResponseEntity.ok().body(tipoUnidadeService.saveTipoUnidade(tipoUnidade));
+  }
+
+  // 6º ENDPOINT
+  @PatchMapping("/{id}")
+  public ResponseEntity<TipoUnidade> atualizarTipoUnidadeParcialmente(@PathVariable int id, @RequestBody TipoUnidade atualizacao) {
+    TipoUnidade dadosAtualizado = tipoUnidadeService.updateParcial(id, atualizacao);
+    return ResponseEntity.ok(dadosAtualizado);
+
   }
 
 }
-  
-
