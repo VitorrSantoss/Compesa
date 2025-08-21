b68e498ddcff536a5722c5409c6444eece9fb31d
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-08-21 16:03:00 -0300

    Implementacao do método patch -> atualizacao parcial

--- src/main/java/compesa/sga/services/TipoControleService.java
+++ src/main/java/compesa/sga/services/TipoControleService.java
@@ -12,26 +12,26 @@ import compesa.sga.model.TipoControle;
 import compesa.sga.repository.TipoControleRepository;
 
 @Service
-public class TipoControleService { 
+public class TipoControleService {
 
   @Autowired
   private TipoControleRepository tipoControleRepository;
 
-  public List<TipoControleDto> GetAllTipoControleService(){
+  public List<TipoControleDto> getAllTipoControleService() {
 
     List<TipoControle> tiposControle = tipoControleRepository.findAll();
 
     List<TipoControleDto> dtos = new ArrayList<>();
 
-    for (TipoControle tipoControle : tiposControle){
+    for (TipoControle tipoControle : tiposControle) {
       dtos.add(tipoControle.converterParaDto());
     }
 
     return dtos;
   }
 
-  public TipoControleDto GetTipoControleById(int id){
-    
+  public TipoControleDto getTipoControleById(int id) {
+
     Optional<TipoControle> tipoControleOpt = tipoControleRepository.findById(id);
 
     if (tipoControleOpt.isPresent()) {
@@ -40,15 +40,32 @@ public class TipoControleService {
     }
     return null;
   }
-  
 
-  public void DeleteTipoControle(int id){
+  public void deleteTipoControle(int id) {
     tipoControleRepository.deleteById(id);
   }
 
-  public TipoControle SaveTipoControle(TipoControle tipoControle){
+  public TipoControle saveTipoControle(TipoControle tipoControle) {
     return tipoControleRepository.save(tipoControle);
   }
 
+  public TipoControle updateParcial(int id, TipoControle atualizacao) {
+    Optional<TipoControle> tipoControleAtual = tipoControleRepository.findById(id);
+
+    // Verificação se o container (optional) possui o objeto "TipoControle"
+    if (tipoControleAtual.isPresent()) {
+      TipoControle tipoControle = tipoControleAtual.get();
+
+      // Aplicação dos campos que foram fornecidos no JSON (postman/swagger)
+      if (atualizacao.getNome() != null) {
+        tipoControle.setNome(atualizacao.getNome());
+      }
+
+      return tipoControleRepository.save(tipoControle);
+    } 
+    else {
+      throw new RuntimeException("TipoControle não encontrado com id: " + id);
+    }
+  }
 
 }
