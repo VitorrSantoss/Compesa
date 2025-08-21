88673660967ff60bfaefb9b9f9c65b8923ec6443
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-08-21 16:02:16 -0300

    Criacao do endpoint PATCH

--- src/main/java/compesa/sga/controller/TipoControleController.java
+++ src/main/java/compesa/sga/controller/TipoControleController.java
@@ -8,76 +8,85 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import compesa.sga.dto.TipoControleDto;
 import compesa.sga.model.TipoControle;
 import compesa.sga.services.TipoControleService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/tipoControle")
 public class TipoControleController {
-  
+
   @Autowired
   private TipoControleService tipoControleService;
 
-  // 1º EndPoint
+  // 1º ENDPOINT
   @GetMapping
-  public ResponseEntity<List<TipoControleDto>> GetTipoControle(){
-    return ResponseEntity.ok().body(tipoControleService.GetAllTipoControleService());
+  public ResponseEntity<List<TipoControleDto>> getTipoControle() {
+    return ResponseEntity.ok().body(tipoControleService.getAllTipoControleService());
   }
 
-  // 2º EndPoint
+  // 2º ENDPOINT
   @GetMapping("/{id}")
-  public ResponseEntity<TipoControleDto> GetById(@PathVariable("id") int id){
-    TipoControleDto tipoControle = tipoControleService.GetTipoControleById(id);
+  public ResponseEntity<TipoControleDto> getById(@PathVariable("id") int id) {
+    TipoControleDto tipoControle = tipoControleService.getTipoControleById(id);
 
-    if (Objects.isNull(tipoControle)){
+    if (Objects.isNull(tipoControle)) {
       return ResponseEntity.notFound().build();
     }
     return ResponseEntity.ok().body(tipoControle);
   }
 
-  // 3º EndPoint
+  // 3º ENDPOINT
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> DeleteTipoControle(@PathVariable("id") int id){
-    TipoControleDto tControleDto = tipoControleService.GetTipoControleById(id);
+  public ResponseEntity<Void> deleteTipoControle(@PathVariable("id") int id) {
+    TipoControleDto tControleDto = tipoControleService.getTipoControleById(id);
 
-    if (Objects.isNull(tControleDto)){
+    if (Objects.isNull(tControleDto)) {
       return ResponseEntity.notFound().build();
     }
 
-    tipoControleService.DeleteTipoControle(id);
+    tipoControleService.deleteTipoControle(id);
 
     return ResponseEntity.noContent().build();
 
   }
 
-  
-  // 4º EndPoint
+  // 4º ENDPOINT
   @PostMapping
-  public ResponseEntity<TipoControle> PostTipoControle(@Valid @RequestBody TipoControle tipoControle){
-    return ResponseEntity.status(HttpStatus.CREATED).body(tipoControleService.SaveTipoControle(tipoControle));
+  public ResponseEntity<TipoControle> PostTipoControle(@Valid @RequestBody TipoControle tipoControle) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(tipoControleService.saveTipoControle(tipoControle));
   }
 
+  // 5º ENDPOINT
   @PutMapping("/{id}")
-  public ResponseEntity<TipoControle> PutTipoControle(@PathVariable("id") int id, @RequestBody TipoControle tipoControle){
-    TipoControleDto tControleDto = tipoControleService.GetTipoControleById(id);
-    if (Objects.isNull(tControleDto)){
+  public ResponseEntity<TipoControle> PutTipoControle(@PathVariable("id") int id,
+      @RequestBody TipoControle tipoControle) {
+    TipoControleDto tControleDto = tipoControleService.getTipoControleById(id);
+    if (Objects.isNull(tControleDto)) {
       return ResponseEntity.noContent().build();
     }
 
     tipoControle.setId(id);
 
-    return ResponseEntity.ok().body(tipoControleService.SaveTipoControle(tipoControle));
+    return ResponseEntity.ok().body(tipoControleService.saveTipoControle(tipoControle));
   }
 
-
+  // 6º ENDPOINT
+  @PatchMapping("/{id}")
+  public ResponseEntity<TipoControle> atualizarTipoControleParcialmente(@PathVariable int id,@RequestBody TipoControle atualizacao) {
+    TipoControle dadosAtualizado = tipoControleService.updateParcial(id, atualizacao);
+    
+    return ResponseEntity.ok(dadosAtualizado);
+  }
 
 }
